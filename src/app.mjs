2141be81bca2db10be64b767389c8c444ad90b71
Ilhamarip <155143109+ilhamganteng321@users.noreply.github.com>
2026-04-20 09:49:03 +0700

    Update app.mjs

--- src/app.mjs
+++ src/app.mjs
@@ -40,7 +40,9 @@ export async function main() {
         }),
         cookie: {
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            httpOnly: true
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
         }
     }));
 
